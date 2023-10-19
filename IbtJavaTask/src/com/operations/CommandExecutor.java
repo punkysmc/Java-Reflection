60744a19270fc50778bbb6044c195c02553edaab
punkysmc <96205087+punkysmc@users.noreply.github.com>
2023-10-19 12:57:46 +0300

    Add files via upload

--- IbtJavaTask/src/com/operations/CommandExecutor.java
+++ IbtJavaTask/src/com/operations/CommandExecutor.java
@@ -6,33 +6,45 @@ import org.hibernate.Session;
 import org.hibernate.cfg.Configuration;
 
 public class CommandExecutor {
-    private Session session = new Configuration().configure().buildSessionFactory().openSession();
+  private Session session;
+  
+  public CommandExecutor() {
+	  
+    session = new Configuration().configure().buildSessionFactory().openSession();
+  }
+  
 
-    public String[] GetClassAndMethodFromDB(String commandName) {
-        String query = "SELECT CLASS_NAME, METHOD FROM COMMAND_EXECUTER WHERE COMMAND_NAME = :COMMAND_NAME";
-        Object[] result = (Object[])this.session.createNativeQuery(query).setParameter("COMMAND_NAME", (Object)commandName).uniqueResult();
-        if (result != null) {
-            return new String[]{(String)result[0], (String)result[1]};
-        }
-        return null;
-    }
+  public String[] GetClassAndMethodFromDB(String commandName) {
+	  
+    String query = "SELECT CLASS_NAME, METHOD FROM COMMAND_EXECUTER WHERE COMMAND_NAME = :COMMAND_NAME";
+    
+    Object[] result = (Object[])session.createNativeQuery(query).setParameter("COMMAND_NAME", commandName).uniqueResult();
+    
 
-    public Boolean ExecuteCommand(String commandName, Object data) throws ClassNotFoundException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        
-    	String[] classAndMethod = this.GetClassAndMethodFromDB(commandName);
-        
-    	String className = classAndMethod[0];
-        String method = classAndMethod[1];
-        
-        Class<?> clazz = Class.forName(className);
-        
-        Method myMethod = clazz.getDeclaredMethod(method, Object.class);
-        myMethod.setAccessible(true);
-        
-        Object instance = clazz.getDeclaredConstructor(new Class[0]).newInstance(new Object[0]);
-        
-        Boolean result = (Boolean)myMethod.invoke(instance, data);
-        
-        return result;
+    if (result != null) {
+    	
+      return new String[] { (String)result[0], (String)result[1] };
     }
+    
+    return null;
+  }
+  
+  public Boolean ExecuteCommand(String commandName, Object data) throws ClassNotFoundException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	  
+    String[] classAndMethod = GetClassAndMethodFromDB(commandName);
+    
+    String className = classAndMethod[0];
+    String method = classAndMethod[1];
+    
+    Class<?> clazz = Class.forName(className);
+    
+    Method myMethod = clazz.getDeclaredMethod(method);
+    myMethod.setAccessible(true);
+    
+    Object instance = clazz.getDeclaredConstructor().newInstance();
+    
+    Boolean result = (Boolean)myMethod.invoke(instance, data);
+    
+    return result;
+  }
 }
--- IbtJavaTask/src/com/operations/CustomerOperations.java
+++ IbtJavaTask/src/com/operations/CustomerOperations.java
@@ -4,84 +4,74 @@ import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.cfg.Configuration;
 
-public class CustomerOperations {
-	
-    private Session session = new Configuration().configure().buildSessionFactory().openSession();
+public class CustomerOperations
+{
+  private Session session;
+  
+  public CustomerOperations() {
+	  
+    session = new Configuration().configure().buildSessionFactory().openSession();
+  }
+  
 
+  private Boolean CustomerAdd(Object data) {
+	  
+    boolean result = false;
     
-    private Boolean CustomerAdd(Object data) {
-    	
-        boolean result = false;
-        
-        try {
-        	
-            this.session.beginTransaction();
-            this.session.persist(data);
-            this.session.getTransaction().commit();
-            result = true;
-            
-        }
-        catch (HibernateException e) {
-        	
-            if (this.session.getTransaction() != null) {
-                this.session.getTransaction().rollback();
-            }
-            
-            e.printStackTrace();
-        }
-        
-        return result;
-        
+    try
+    {
+      session.beginTransaction();
+      session.persist(data);
+      session.getTransaction().commit();
+      result = true;
+    }
+    catch (HibernateException e)
+    {
+      if (session.getTransaction() != null) session.getTransaction().rollback();
+      e.printStackTrace();
     }
     
+    return result;
+  }
+  
 
-    private Boolean CustomerEdit(Object data) {
-    	
-        boolean result = false;
-        
-        try {
-        	
-            this.session.beginTransaction();
-            this.session.merge(data);
-            this.session.getTransaction().commit();
-            result = true;
-            
-        }
-        catch (HibernateException e) {
-        	
-            if (this.session.getTransaction() != null) {
-                this.session.getTransaction().rollback();
-            }
-            
-            e.printStackTrace();
-        }
-        
-        return result;
-        
+  private Boolean CustomerEdit(Object data) {
+	  
+    boolean result = false;
+    
+    try
+    {
+      session.beginTransaction();
+      session.merge(data);
+      session.getTransaction().commit();
+      result = true;
+    }
+    catch (HibernateException e)
+    {
+      if (session.getTransaction() != null) session.getTransaction().rollback();
+      e.printStackTrace();
     }
     
-
-    private Boolean CustomerDelete(Object data) {
-    	
-        boolean result = false;
-        
-        try {
-            this.session.beginTransaction();
-            this.session.remove(data);
-            this.session.getTransaction().commit();
-            result = true;
-            
-        }
-        catch (HibernateException e) {
-        	
-            if (this.session.getTransaction() != null) {
-                this.session.getTransaction().rollback();
-            }
-            
-            e.printStackTrace();
-        }
-        
-        return result;
-        
+    return result;
+  }
+  
+  private Boolean CustomerDelete(Object data) {
+	  
+    boolean result = false;
+    
+    try
+    {
+      session.beginTransaction();
+      session.remove(data);
+      session.getTransaction().commit();
+      result = true;
     }
+    catch (HibernateException e)
+    {
+      if (session.getTransaction() != null) session.getTransaction().rollback();
+      e.printStackTrace();
+    }
+    
+    return result;
+  }
 }
